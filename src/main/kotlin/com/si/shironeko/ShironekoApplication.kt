c8cadfefabdd7e8b9357be3350d2759a473bac27
si-vo-hung <vo.hung@sharing-innovations.com>
2020-07-15 16:58:37 +0700

    initial

---
+++ src/main/kotlin/com/si/shironeko/ShironekoApplication.kt
@@ -0,0 +1,11 @@
+package com.si.shironeko
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShironekoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShironekoApplication>(*args)
+}
